3fec9d10d0a61464c83690f9e88be48fc1d198cf
Rodrigo Garcia da Silva <rgs190385@gmail.com>
2022-02-19 16:00:43 -0300

    desafio de consoantes

--- src/desafios/consoantes.java
+++ src/desafios/consoantes.java
@@ -1,2 +1,47 @@
-package desafios;public class consoantes {
+package desafios;
+/*Faça um programa que leia o vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
+
+//import java.awt.print.Printable;
+//import java.security.Principal;
+import java.util.Scanner;
+
+public class consoantes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeConsoantes = 0;
+
+        int count = 0;
+        do {
+            System.out.print("Letra: ");
+            String letra = sc.next();
+
+            if (letra.equalsIgnoreCase("a") ||
+                    letra.equalsIgnoreCase("e") ||
+                    letra.equalsIgnoreCase("i") ||
+                    letra.equalsIgnoreCase("o") ||
+                    letra.equalsIgnoreCase("u")) ;
+            {
+                consoantes[count] = letra;
+                quantidadeConsoantes++;
+            }
+
+            count++;
+
+        } while (count < consoantes.length);
+
+        System.out.println("consoantes: ");
+
+
+        for (String consoante : consoantes) {
+            if (consoante != null)
+                System.out.println(consoante + " ");
+
+        }
+    }
 }
+
