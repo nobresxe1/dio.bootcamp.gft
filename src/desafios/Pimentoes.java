82cf33e86761b3b5c282ce5036909c8ad460a3c4
Rodrigo Garcia da Silva <rgs190385@gmail.com>
2022-02-19 15:55:53 -0300

    desafio onde precisava somar

---
+++ src/desafios/Pimentoes.java
@@ -0,0 +1,20 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class Pimentoes {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+
+        int a = leitor.nextInt();
+        int b = leitor.nextInt();
+
+        //digite o seu código
+        int x = a + b;
+
+        System.out.print("X= "+ x);
+    }
+
+}
